db1bdcf372c97398bfac478194f48771525949da
saimedhi <saimedhi@amazon.com>
2024-03-06 11:04:11 -0800

    Introduced service time metrics to OpenSearch-Py client.
    Signed-off-by: saimedhi <saimedhi@amazon.com>

--- opensearchpy/metrics.py
+++ opensearchpy/metrics.py
@@ -18,7 +18,7 @@ class TimeMetrics:
         self.events = Events()
         self.start_time = None
         self.end_time = None
-        self.service_time = None
+        self.service_time = 0
 
         # Subscribe to the server_request_start and server_request_end events
         self.events.server_request_start += self.server_request_start
