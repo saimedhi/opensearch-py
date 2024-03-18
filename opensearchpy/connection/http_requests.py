bfe16e1c91a4a0002d47228b4b39d662f1f1ea59
Sai Medhini Reddy Maryada <117196660+saimedhi@users.noreply.github.com>
2024-03-17 23:34:47 -0700

    Update http_requests.py

--- opensearchpy/connection/http_requests.py
+++ opensearchpy/connection/http_requests.py
@@ -27,7 +27,9 @@
 
 import time
 import warnings
-from typing import Any, Collection, Mapping, Optional, Union
+from typing import Any, Collection, Mapping, Optional, Union, Type
+
+from opensearchpy.metrics import Metrics, MetricsEvents
 
 try:
     import requests
@@ -86,8 +88,10 @@ class RequestsHttpConnection(Connection):
         http_compress: Any = None,
         opaque_id: Any = None,
         pool_maxsize: Any = None,
+        metrics: Type[Metrics] = None,
         **kwargs: Any
     ) -> None:
+        self.metrics = metrics
         if not REQUESTS_AVAILABLE:
             raise ImproperlyConfigured(
                 "Please install requests to use RequestsHttpConnection."
@@ -176,7 +180,6 @@ class RequestsHttpConnection(Connection):
             body = self._gzip_compress(body)
             headers["content-encoding"] = "gzip"  # type: ignore
 
-        start = time.time()
         request = requests.Request(method=method, headers=headers, url=url, data=body)
         prepared_request = self.session.prepare_request(request)
         settings = self.session.merge_environment_settings(
@@ -187,9 +190,14 @@ class RequestsHttpConnection(Connection):
             "allow_redirects": allow_redirects,
         }
         send_kwargs.update(settings)
+        self.is_metrics_none = False
+        if self.metrics is None:
+            self.metrics=MetricsEvents()
+            self.is_metrics_none = True
         try:
+            self.metrics.request_start()
             response = self.session.send(prepared_request, **send_kwargs)
-            duration = time.time() - start
+            self.metrics.request_end()
             raw_data = response.content.decode("utf-8", "surrogatepass")
         except reraise_exceptions:
             raise
@@ -199,7 +207,7 @@ class RequestsHttpConnection(Connection):
                 url,
                 prepared_request.path_url,
                 orig_body,
-                time.time() - start,
+                time.perf_counter() - self.metrics.start_time,
                 exception=e,
             )
             if isinstance(e, requests.exceptions.SSLError):
@@ -224,7 +232,7 @@ class RequestsHttpConnection(Connection):
                 url,
                 response.request.path_url,
                 orig_body,
-                duration,
+                self.metrics.service_time,
                 response.status_code,
                 raw_data,
             )
@@ -241,10 +249,14 @@ class RequestsHttpConnection(Connection):
             orig_body,
             response.status_code,
             raw_data,
-            duration,
+            self.metrics.service_time,
         )
 
-        return response.status_code, response.headers, raw_data
+        if self.is_metrics_none:
+            self.metrics=None
+            return response.status_code,response.headers,raw_data
+        else:
+            return response.status_code, response.headers, raw_data, self.metrics.service_time
 
     @property
     def headers(self) -> Any:  # type: ignore
