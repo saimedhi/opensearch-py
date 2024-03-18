14e7ba09fe0d2e9af953852d9598d0f70b89fcac
saimedhi <saimedhi@amazon.com>
2024-03-18 15:26:02 -0700

    Introduced  metrics to opensearch-py client
    Signed-off-by: saimedhi <saimedhi@amazon.com>

--- opensearchpy/metrics.py
+++ opensearchpy/metrics.py
@@ -1,13 +1,3 @@
-# SPDX-License-Identifier: Apache-2.0
-#
-# The OpenSearch Contributors require contributions made to
-# this file be licensed under the Apache-2.0 license or a
-# compatible open source license.
-#
-# Modifications Copyright OpenSearch Contributors. See
-# GitHub history for details.
-
-
 import time
 from abc import ABC, abstractmethod
 
@@ -15,9 +5,6 @@ from events import Events
 
 
 class Metrics(ABC):
-    def __init__(self) -> None:
-        self.service_time = 0.0
-
     @abstractmethod
     def request_start(self) -> None:
         pass
@@ -26,11 +13,30 @@ class Metrics(ABC):
     def request_end(self) -> None:
         pass
 
+    @property
+    @abstractmethod
+    def start_time(self) -> float:
+        pass
+
+    @property
+    @abstractmethod
+    def service_time(self) -> float:
+        pass
+
 
 class MetricsEvents(Metrics):
+    @property
+    def start_time(self) -> float:
+        return self._start_time
+
+    @property
+    def service_time(self) -> float:
+        return self._service_time
+
     def __init__(self) -> None:
-        super().__init__()
         self.events = Events()
+        self._start_time = 0.0
+        self._service_time = 0.0
 
         # Subscribe to the request_start and request_end events
         self.events.request_start += self._on_request_start
@@ -40,11 +46,11 @@ class MetricsEvents(Metrics):
         self.events.request_start()
 
     def _on_request_start(self) -> None:
-        self.start_time = time.perf_counter()
+        self._start_time = time.perf_counter()
 
     def request_end(self) -> None:
         self.events.request_end()
 
     def _on_request_end(self) -> None:
-        self.end_time = time.perf_counter()
-        self.service_time = self.end_time - self.start_time
+        self._end_time = time.perf_counter()
+        self._service_time = self._end_time - self._start_time
